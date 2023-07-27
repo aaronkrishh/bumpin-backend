1ca3a682b679a2762f870a42dfd07df2244a6776
hadialhakeem <hadihakeem2@gmail.com>
2023-07-27 14:25:05 -0400

    Refactored schema, removed player model

--- prisma/migrations/20230727182427_init/migration.sql
+++ prisma/migrations/20230727182427_init/migration.sql
@@ -11,7 +11,7 @@ CREATE TYPE "WinnerTeam" AS ENUM ('A', 'B');
 CREATE TYPE "Pool" AS ENUM ('A', 'B', 'C', 'D');
 
 -- CreateEnum
-CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE');
+CREATE TYPE "UserRole" AS ENUM ('STANDARD', 'ADMIN');
 
 -- CreateTable
 CREATE TABLE "Tournament" (
@@ -58,17 +58,19 @@ CREATE TABLE "Team" (
 );
 
 -- CreateTable
-CREATE TABLE "Player" (
+CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "teamId" INTEGER NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-    "gender" "Gender" NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- AddForeignKey
 ALTER TABLE "Score" ADD CONSTRAINT "Score_tournamentId_fkey" FOREIGN KEY ("tournamentId") REFERENCES "Tournament"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -81,6 +83,3 @@ ALTER TABLE "Score" ADD CONSTRAINT "Score_teamBId_fkey" FOREIGN KEY ("teamBId") 
 
 -- AddForeignKey
 ALTER TABLE "Team" ADD CONSTRAINT "Team_tournamentId_fkey" FOREIGN KEY ("tournamentId") REFERENCES "Tournament"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Player" ADD CONSTRAINT "Player_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

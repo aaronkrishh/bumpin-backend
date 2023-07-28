3a56ab3e3ecefd637797b313fcd186abefd87976
hadialhakeem <hadihakeem2@gmail.com>
2023-07-27 23:44:11 -0400

    fixed err message

--- src/utils/seeding.ts
+++ src/utils/seeding.ts
@@ -17,7 +17,7 @@ async function computePlayoffSeeding(tournamentId: number) {
 
     scores.forEach(score => {
         if (score.teamA.pool !== score.teamB.pool) {
-            console.error(`Pool Play scores exist between teams in different pools. Score ID: ${score.id}`)
+            throw new Error(`Pool Play scores exist between teams in different pools. Score ID: ${score.id}`)
         }
         if (score.winner === "A") {
             record[score.teamA.id].setsWon += 1
